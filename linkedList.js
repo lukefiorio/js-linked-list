899bf1013459728a40387ece896c75a03c5de94e
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-04 17:07:18 -1000

    add func

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,44 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
+function linkedListGenerator() {
 
+  let head = null;
+  let tail = null;
+
+  const getHead = function () {
+    return head;
+  }
+  const getTail = function () {
+    // if (obj) {
+    //   while (obj.next !== null) {
+    //     nextObj = obj.next;
+    //     obj = nextObj;
+    //   }
+    // return nextObj;
+    // }
+    return tail;
+  }
+
+  const add = function (val) {
+    let newNode = {};
+    newNode.value = val;
+    newNode.next = null;
+    tail = newNode;
+    if (!head) head = newNode;
+    return newNode;
+  }
+
+  const remove = function () { }
+  const get = function () { }
+  const insert = function () { }
+
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    remove: remove,
+    get: get,
+    insert: insert
+  };
 }
